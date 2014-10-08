a82cbaf3e26f5a50e9296d29c7813f07f241eefd
Jian Yuan Lee <jianyuan@gmail.com>
2014-10-08 15:41:26 +0100

    Add endpoint settings

--- bluesnap/client.py
+++ bluesnap/client.py
@@ -1,3 +1,15 @@
 class BluesnapClient(object):
-    def __init__(self):
-        pass
+    ENDPOINTS = {
+        'live': 'https://ws.bluesnap.com',
+        'sandbox': 'https://sandbox.bluesnap.com'
+    }
+
+    def __init__(self, env):
+        if env not in self.ENDPOINTS:
+            raise ValueError('env not in {0}'.format(self.ENDPOINTS.keys()))
+
+        self.env = env
+
+    @property
+    def endpoint_url(self):
+        return self.ENDPOINTS[self.env]
--- tests/test_client.py
+++ tests/test_client.py
@@ -5,5 +5,11 @@ from bluesnap.client import BluesnapClient
 
 class BluesnapClientTestCase(unittest.TestCase):
     def setUp(self):
-        self.client = BluesnapClient()
+        self.client = BluesnapClient(env='live')
 
+    def test_env(self):
+        self.assertEqual(BluesnapClient.ENDPOINTS.keys(), ['live', 'sandbox'])
+
+        for env, endpoint_url in BluesnapClient.ENDPOINTS.iteritems():
+            client = BluesnapClient(env=env)
+            self.assertEqual(client.endpoint_url, endpoint_url)
