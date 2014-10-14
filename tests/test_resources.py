15df21926e2356fa720eaceef4b61035c3e5bd0f
Jian Yuan Lee <jianyuan@gmail.com>
2014-10-14 21:58:11 +0100

    Add find_by_seller_shopper_id unit test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,8 +1,9 @@
 import unittest
 
+from mock import MagicMock
+
 from bluesnap.models import ContactInfo, PlainCreditCard
 from bluesnap.resources import Order, Shopper
-
 from helper import configure_client, DUMMY_CARD_VISA
 
 
@@ -11,31 +12,41 @@ configure_client()
 
 class ShopperTestCase(unittest.TestCase):
     def setUp(self):
-        pass
-
-    def test_shopper_xml(self):
-        contact_info = ContactInfo(
+        self.contact_info = ContactInfo(
             first_name='John',
             last_name='Doe',
             email='test@justyoyo.com',
             zip='SW5',
             country='gb',
-            phone='07777777777'
-        )
+            phone='07777777777')
 
-        credit_card = PlainCreditCard(
+        self.credit_card = PlainCreditCard(
             card_type=DUMMY_CARD_VISA['card_type'],
             expiration_month=DUMMY_CARD_VISA['expiration_month'],
             expiration_year=DUMMY_CARD_VISA['expiration_year'],
             card_number=DUMMY_CARD_VISA['card_number'],
-            security_code=DUMMY_CARD_VISA['security_code']
-        )
+            security_code=DUMMY_CARD_VISA['security_code'])
 
+    def test_shopper_xml(self):
         shopper = Shopper()
         shopper.create(
-            contact_info=contact_info,
-            credit_card=credit_card)
-        self.assertTrue(False)
+            contact_info=self.contact_info,
+            credit_card=self.credit_card)
+        self.assertTrue(False)  # TODO
 
-    # def test_order_xml(self):
-    #     order = Order().create()
+    def test_find_by_with_valid_shopper_id(self):
+        pass  # TODO
+
+    def test_find_by_seller_shopper_id(self):
+        seller_id = 'seller_id'
+        seller_shopper_id = 'seller_shopper_id'
+
+        # Set up
+        shopper = Shopper()
+        shopper.client.seller_id = seller_id
+        shopper.find_by_shopper_id = MagicMock()
+
+        shopper.find_by_seller_shopper_id(seller_shopper_id)
+        shopper.find_by_shopper_id.assert_called_once_with('{seller_shopper_id},{seller_id}'.format(
+            seller_shopper_id=seller_shopper_id,
+            seller_id=seller_id))
