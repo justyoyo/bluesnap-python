2ceb4f7195220d52ce92156da9332b50369fb746
Jian Yuan Lee <jianyuan@gmail.com>
2014-10-15 11:25:26 +0100

    Return formatted messages in APIError

--- bluesnap/exceptions.py
+++ bluesnap/exceptions.py
@@ -1,5 +1,10 @@
 class APIError(Exception):
-    pass
+    def __init__(self, messages):
+        self.messages = messages
+
+    def __str__(self):
+        import json
+        return json.dumps(self.messages, indent=2)
 
 
 class ImproperlyConfigured(Exception):
