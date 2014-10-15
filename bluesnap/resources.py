edbb6f33acdc41f27579b51dea8863869289a521
Jian Yuan Lee <jianyuan@gmail.com>
2014-10-15 11:55:13 +0100

    Refactor

--- bluesnap/resources.py
+++ bluesnap/resources.py
@@ -28,47 +28,6 @@ class Shopper(Resource):
     new_shopper_path = path + '/{shopper_id}'
     shopper_id_path_pattern = re.compile('{}/(\d+)'.format(path))  # /services/2/shoppers/(\d+)
 
-    def create(self, contact_info, credit_card=None, seller_shopper_id=None):
-        """
-        :type contact_info : models.ContactInfo
-        :type credit_card : models.AbstractCreditCard
-        """
-        # noinspection PyPep8Naming
-        E = self.client.E
-
-        credit_cards_info = []
-        if credit_card is not None:
-            credit_cards_info.append(getattr(E, 'credit-card-info')(
-                contact_info.to_xml('billing'),
-                credit_card.to_xml()
-            ))
-
-        shopper_info = []
-        if seller_shopper_id is not None:
-            shopper_info.append(getattr(E, 'seller-shopper-id')(seller_shopper_id))
-
-        # noinspection PyCallByClass
-        shopper_element = E.shopper(
-            getattr(E, 'shopper-info')(
-                getattr(E, 'store-id')(self.client.store_id),
-                getattr(E, 'shopper-currency')(self.client.currency),
-                E.locale(self.client.locale),
-                contact_info.to_xml('shopper'),
-                getattr(E, 'payment-info')(
-                    getattr(E, 'credit-cards-info')(*credit_cards_info)
-                ),
-                *shopper_info
-            ),
-            models.WebInfo().to_xml()
-        )
-
-        response, body = self.request('POST', self.path, data=etree.tostring(shopper_element))
-
-        new_shopper_url = urlparse(response.headers['location'])
-
-        shopper_id = self.shopper_id_path_pattern.match(new_shopper_url.path).group(1)
-        return self.find_by_shopper_id(int(shopper_id))
-
     def find_by_shopper_id(self, shopper_id):
         """
         :raise self.DoesNotExist
@@ -87,6 +46,52 @@ class Shopper(Resource):
         return self.find_by_shopper_id('{seller_shopper_id},{seller_id}'.format(
             seller_shopper_id=seller_shopper_id,
             seller_id=self.client.seller_id))
+
+    def _create_shopper_element(self, contact_info, credit_card=None, seller_shopper_id=None):
+        # noinspection PyPep8Naming
+        E = self.client.E
+
+        credit_cards_info = []
+        if credit_card is not None:
+            credit_cards_info.append(getattr(E, 'credit-card-info')(
+                contact_info.to_xml('billing'),
+                credit_card.to_xml()
+            ))
+
+        shopper_info = []
+        if seller_shopper_id is not None:
+            shopper_info.append(getattr(E, 'seller-shopper-id')(seller_shopper_id))
+
+        return E.shopper(
+            getattr(E, 'shopper-info')(
+                getattr(E, 'store-id')(self.client.store_id),
+                getattr(E, 'shopper-currency')(self.client.currency),
+                E.locale(self.client.locale),
+                contact_info.to_xml('shopper'),
+                getattr(E, 'payment-info')(
+                    getattr(E, 'credit-cards-info')(*credit_cards_info)
+                ),
+                *shopper_info
+            ),
+            models.WebInfo().to_xml()
+        )
+
+    def create(self, contact_info, credit_card=None, seller_shopper_id=None):
+        """
+        :type contact_info : models.ContactInfo
+        :type credit_card : models.AbstractCreditCard
+        """
+        shopper_element = self._create_shopper_element(contact_info, credit_card, seller_shopper_id)
+
+        response, body = self.request('POST', self.path, data=etree.tostring(shopper_element))
+
+        new_shopper_url = urlparse(response.headers['location'])
+
+        shopper_id = self.shopper_id_path_pattern.match(new_shopper_url.path).group(1)
+        return self.find_by_shopper_id(int(shopper_id))
+
+    def update(self, contact_info, credit_card=None):
+        shopper_element = self._create_shopper_element(contact_info, credit_card)
 
 
 class Order(Resource):
